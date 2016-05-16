5082dfda368d42ebe7be322172cfb818fd41d023
Jouba Ayedi <joubausa@icloud.com>
2016-05-16 06:25:22 -0700

    Initial Commit

---
+++ DreamCatcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DreamCatcher
+//
+//  Created by Mohamed Ayadi on 5/16/16.
+//  Copyright © 2016 Mohamed Ayadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DreamCatcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DreamCatcher
+//
+//  Created by Mohamed Ayadi on 5/16/16.
+//  Copyright © 2016 Mohamed Ayadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
